53e9319f4ef1747d1ef672c2ebc0ea9101f7b500
Anna <166062978+AnnaLazare@users.noreply.github.com>
2024-04-13 15:52:36 +0300

    Вынесли блок Header в отдельный компонент

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <main>
+          <p>Внесли изменения сайта. Добвили блок main</p>
+      </main>
+
+    </div>
+  );
+}
+
+export default App;
